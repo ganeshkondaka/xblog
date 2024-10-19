5c4c0f1041657f97b8f6efd6ad1631d9c7996823
ganeshkondaka <kondakaganesh2003@outlook.com>
2024-10-19 20:26:05 +0530

    added some stylinh to the blog demo comp

--- frontend/src/components/Blogdemo.tsx
+++ frontend/src/components/Blogdemo.tsx
@@ -9,20 +9,20 @@ export const Blogdemo = ({ blog }: BlogdemoProps) => {
     <div className="mb-5">
 
       <div className="">
-        <div className="flex itmes-center">
-          <div className="bg-green-700 h-10 w-10 rounded-full text-center text-2xl text-white">{(blog.author.name).slice(0,1)} </div>
-          <span className="flex items-center pl-4">{blog.author.name}</span>
+        <div className="flex itmes-center pb-4">
+          <div className="bg-green-700 h-6 w-6 rounded-full text-center text-1xl text-white flex justify-center  ">{(blog.author.name).slice(0,1)} </div>
+          <div className="flex items-center pl-4 text-slate-800">{blog.author.name}</div>
         </div>
-        <h1>
+        <h1 className="text-2xl font-bold pl-1">
           {blog.title}
         </h1>
-        <div>
+        <div className="text-gray-500 mb-4">
           {(blog.content).length > 100 ? ((blog.content).slice(0, 100)+'...') : blog.content}
         </div>
-        <div>
-          Date : {(blog.createdAt).slice(0, 10)} and extras 'like button'
+        <div className="text-gray-600 mb-10">
+          {(blog.createdAt).slice(0, 10)}     and extras 'like button'
         </div>
-        <hr className="border border-red-600" />
+        <hr className="border border-gray-100" />
       </div>
     </div>
   )
